19b04b128dc62424b4a1c4948b5d727ab290bf98
Alexander Ryabishkin <sintes4ek@gmail.com>
2022-10-24 12:36:48 +0300

    Add category data parsing.

--- bonjuredv_api.py
+++ bonjuredv_api.py
@@ -65,6 +65,46 @@ def _extract_categories_ids(response: requests.Response) -> list[int]:
     return result
 
 
+def get_category_product_data(category_id: int) -> dict:
+    """Fucntion requests to bonjure-dv API and 
+    gets json with category product data."""
+
+    headers = {
+        'User-Agent': UserAgent().random,
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        # 'Accept-Encoding': 'gzip, deflate, br',
+        'Origin': 'https://bonjour-dv.ru',
+        'Connection': 'keep-alive',
+        'Referer': 'https://bonjour-dv.ru/',
+        # 'Cookie': 'PHPSESSID=nibfu1qkst0hurul7jm4d8k9kf',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+    }
+
+    params = {
+        'filter[categories]': category_id,
+        'offset': '0',
+        'resourceId': category_id,
+    }
+
+    parse_attempts = config.PARSE_ATTEMPTES
+    parse_timeout = config.PARSE_TIMEOUT
+    while parse_attempts > 0:
+        parse_attempts -= 1
+        try:
+            response = requests.get('https://api.bonjour-dv.ru/public_v1/products', params=params, headers=headers)
+            parse_attempts = 0
+        except Exception as e:
+            if parse_attempts == 0:
+                raise e
+            time.sleep(parse_timeout)
+    _delay()
+
+    return response.json()
+
+
 def main() -> None:
     pass
 
